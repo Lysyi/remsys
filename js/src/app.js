d3b884329224322810a520d1ed685a0f82e9ea73
Sasha <oleksandr.lysyi@gmail.com>
2015-06-03 10:01:26 +0300

    content

--- js/src/app.js
+++ js/src/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+$(document).ready( function(){
+  $(".main-menu-clicker").click(function(){
+    var toggleWidth = "0px";
+    if($(".main-menu-container").width() === 280)
+    {
+      toggleWidth="30px";
+      $(this).addClass('active');
+    } else {
+      toggleWidth="280px";
+      $(this).removeClass('active');
+    }
+    $('.main-menu-container').animate({ width: toggleWidth });
+  });
+});
